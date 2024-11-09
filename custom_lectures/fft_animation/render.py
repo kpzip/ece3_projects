ce2ee8fee66595df80bc89a42704f855865acb3d
kpzip <kpzipgames@gmail.com>
2024-11-09 13:30:36 -0800

    rename first scene

--- custom_lectures/fft_animation/render.py
+++ custom_lectures/fft_animation/render.py
@@ -7,7 +7,7 @@ def render():
         quality = 'h'
     if '-4k' in sys.argv:
         quality = 'k'
-    os.system(f"manim -pq{quality} scene.py SineWaves")
+    os.system(f"manim -pq{quality} intro_freq_what_is_ft.py SineWaves")
 
 if __name__ == '__main__':
     render()
