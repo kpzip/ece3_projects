426d8efd18172b1046f1284ea4f357d16b509a16
kpzip <kpzipgames@gmail.com>
2024-11-09 18:45:17 -0800

    Create lib.py

---
+++ custom_lectures/fft_animation/lib.py
@@ -0,0 +1,18 @@
+from typing import Callable
+from matplotlib import pyplot as plt
+import math
+import numpy as np
+
+def square_wave_fourier(n: int) -> Callable:
+    return lambda x: sum([(4/((2 * n2 + 1) * np.pi)) * math.sin((2 * n2 + 1) * np.pi * x) for n2 in range(n)])
+
+
+def test():
+    x_range = np.linspace(0, 2, 100)
+    fourier_5 = square_wave_fourier(5)
+    y_range = [fourier_5(y) for y in x_range]
+    plt.plot(x_range, y_range)
+    plt.show()
+
+if __name__ == '__main__':
+    test()
