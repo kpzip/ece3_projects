51052634f3e1a4de2eeac93e0c81ed62c8d61d8c
kpzip <kpzipgames@gmail.com>
2024-11-12 17:09:53 -0800

    Update ft_explanation.py

--- custom_lectures/fft_animation/ft_explanation.py
+++ custom_lectures/fft_animation/ft_explanation.py
@@ -6,7 +6,7 @@ class FourierTransformInternals(Scene):
 
     def construct(self):
         ft_intro_text = Text("How It Works", gradient=(RED, BLUE)).scale(2)
-        transform_tex_initial_animation = MathTex(r"\hat{f}(\xi)", r"=", r"\int_{-\infty}^{\infty}", r"f(x)", r"e^{-2\pi i", r"x", r"\xi}", r"dx", color=WHITE).shift(3 * RIGHT)
+        transform_tex_initial_animation = MathTex(r"\hat{f}(", r"\xi", r")", r"=", r"\int_{-\infty}^{\infty}", r"f(x)", r"e^{-2\pi i", r"x", r"\xi}", r"dx", color=WHITE).shift(3 * RIGHT)
         complex_plane_initial_animation = ComplexPlane(x_range=[-1.3, 1.3, 1], y_range=[-1.3, 1.3, 1], x_length=6, y_length=6).add_coordinates().shift(3 * LEFT)
         transform_tex_scale = 0.7
         transform_tex_shift = 1 * RIGHT + 1.2 * DOWN
@@ -14,23 +14,31 @@ class FourierTransformInternals(Scene):
         complex_plane_scale = 0.5
         complex_plane_shift = 1 * LEFT + 1.2 * DOWN
         complex_plane = complex_plane_initial_animation.copy().scale(complex_plane_scale).shift(complex_plane_shift)
-        axes_max_x = 10
-        axes = Axes(x_range=[0, axes_max_x, 1], y_range=[0, 1.2, 1], x_length=25, y_length=5, axis_config={"include_numbers": True}).scale(0.4).shift(2.5 * UP)
+        axes_max_x = 5
+        axes = Axes(x_range=[0, axes_max_x, 0.5], y_range=[0, 1.2, 1], x_length=25, y_length=5, axis_config={"include_numbers": True}, x_axis_config={"numbers_with_elongated_ticks": range(0, axes_max_x, 1)}).scale(0.4).shift(2.5 * UP)
 
-        sin_wave_func = lambda x: 0.5 * np.cos(x * 2 * np.pi) + 0.5
+        sin_wave_func = lambda x: 0.5 * np.cos(x * 2 * np.pi * 2) + 0.5
         sin_wave_draw_tracker = ValueTracker(0)
         sin_wave_cartesian_graph_drawing = always_redraw(lambda: axes.plot(sin_wave_func, x_range=[0, sin_wave_draw_tracker.get_value()], color=RED))
         sin_wave_cartesian_arrow = always_redraw(lambda: Arrow(start=axes.c2p(sin_wave_draw_tracker.get_value(), 0), end=axes.c2p(sin_wave_draw_tracker.get_value(), sin_wave_func(sin_wave_draw_tracker.get_value())), buff=0, color=YELLOW))
         sin_wave_cartesian_graph = axes.plot(sin_wave_func, color=RED)
 
-        sin_wave_radial_graph = complex_plane.plot_polar_graph(sin_wave_func, theta_range=[0, 3*PI], color=RED)
+        xi_tracker_init = 1
+        xi_tracker = ValueTracker(xi_tracker_init)
+        xi_tex = always_redraw(lambda: MathTex(r"\xi=", f"{xi_tracker.get_value():.2f}").scale(0.6).shift(2 * DOWN))
+        xi_slider = NumberLine(x_range=[0, 5, 1], include_numbers=True, numbers_with_elongated_ticks=[0, 5]).scale(0.7).shift(2.4 * DOWN)
+        xi_dot = always_redraw(lambda: Dot(point=xi_slider.n2p(xi_tracker.get_value())))
+
+        theta_range = np.array([0, (16 * PI)])
+        sin_wave_radial_graph = complex_plane.plot_polar_graph(lambda x: sin_wave_func(x / (xi_tracker_init * 2 * PI)), theta_range=theta_range, color=RED)
+        sin_wave_radial_graph_adjustable = always_redraw(lambda: complex_plane.plot_polar_graph(lambda x: sin_wave_func(x / (xi_tracker.get_value() * 2 * PI)), theta_range=theta_range * xi_tracker.get_value(), color=RED))
 
         arrow_tracker = ValueTracker(0)
-        drawable_arrow = Arrow(start=complex_plane.get_origin(), end=complex_plane.c2p(1, 0), color=YELLOW, buff=0)
+
         arrow = always_redraw(lambda: Arrow(start=complex_plane.get_origin(), end=complex_plane.c2p(np.cos(arrow_tracker.get_value() * 2 * np.pi), np.sin(arrow_tracker.get_value() * 2 * np.pi)), color=YELLOW, buff=0))
         sin_following_arrow = always_redraw(lambda: Arrow(start=complex_plane.get_origin(), end=complex_plane.c2p(
-            np.cos(arrow_tracker.get_value() * 2 * np.pi) * sin_wave_func(arrow_tracker.get_value() * 2 * PI),
-            np.sin(arrow_tracker.get_value() * 2 * np.pi) * sin_wave_func(arrow_tracker.get_value() * 2 * PI)),
+            np.cos(arrow_tracker.get_value() * 2 * np.pi) * sin_wave_radial_graph.underlying_function(arrow_tracker.get_value() * 2 * PI),
+            np.sin(arrow_tracker.get_value() * 2 * np.pi) * sin_wave_radial_graph.underlying_function(arrow_tracker.get_value() * 2 * PI)),
                                             color=YELLOW, buff=0))
         complex_unit_circle = Circle.from_three_points(complex_plane.c2p(1, 0), complex_plane.c2p(-1, 0), complex_plane.c2p(0, 1), color=PURPLE)
 
@@ -49,11 +57,9 @@ class FourierTransformInternals(Scene):
         self.wait(2)
 
         # Arrow rotating - euler's formula
-        self.play(Circumscribe(transform_tex[4]), run_time=2)
+        self.play(Circumscribe(transform_tex[6]), run_time=2)
         self.wait(2)
-        self.play(Write(drawable_arrow))
-        self.remove(drawable_arrow)
-        self.add(arrow)
+        self.play(Write(arrow))
         self.wait(0.5)
         rev_time = 2.5
         self.play(arrow_tracker.animate.set_value(1), run_time=rev_time, rate_func=linear)
@@ -64,7 +70,7 @@ class FourierTransformInternals(Scene):
         self.wait(1)
 
         # Radial Graphing + Sin Wave
-        self.play(Circumscribe(transform_tex[3]), Circumscribe(transform_tex[5]), run_time=2)
+        self.play(Circumscribe(transform_tex[5]), Circumscribe(transform_tex[7]), run_time=2)
         self.play(Write(axes))
         self.play(FadeIn(sin_wave_cartesian_arrow))
         self.add(sin_wave_cartesian_graph_drawing)
@@ -80,8 +86,34 @@ class FourierTransformInternals(Scene):
         self.add(sin_wave_radial_graph)
         self.remove(arrow)
         self.add(sin_following_arrow)
-        self.play(arrow_tracker.animate.set_value(1.5), run_time=(rev_time * 1.5) * 1.5, rate_func=linear)
+        self.play(arrow_tracker.animate.set_value(1), run_time=rev_time * 1.5, rate_func=linear)
         self.play(FadeOut(sin_following_arrow))
+        self.wait(1)
+
+        # xi
+        self.play(Circumscribe(transform_tex[8]))
+        self.remove(sin_wave_radial_graph)
+        self.add(sin_wave_radial_graph_adjustable)
+        self.play(Write(xi_tex))
+        self.play(Write(xi_slider), Write(xi_dot))
+        self.play(xi_tracker.animate.set_value(2), run_time=4)
+        self.wait(0.5)
+        self.play(xi_tracker.animate.set_value(3), run_time=3)
+        self.wait(0.5)
+        self.play(xi_tracker.animate.set_value(0.5), run_time=6)
+        self.wait(0.5)
+        self.play(xi_tracker.animate.set_value(2), run_time=2)
+        brace1 = BraceBetweenPoints(axes.c2p(1, 1), axes.c2p(1.5, 1))
+        brace2 = BraceBetweenPoints(axes.c2p(1.5, 1), axes.c2p(2, 1))
+        freq_text = MathTex(r"\text{2 Hz}").scale(0.6).move_to(axes.c2p(1.5, 1.4))
+        self.play(Write(brace1))
+        self.play(Write(brace2))
+        self.play(Write(freq_text))
+        self.wait(1)
+        self.play(Unwrite(freq_text))
+        self.play(Unwrite(brace1, brace2))
+        self.wait(0.5)
+        self.play(xi_tracker.animate.set_value(1), run_time=2)
 
 
         # Wait at the end
