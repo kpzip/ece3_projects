61c9fe533904166bb4170f256cad7d1e1ce22571
kpzip <kpzipgames@gmail.com>
2024-11-23 01:26:57 -0800

    work

--- custom_lectures/fft_animation/render.py
+++ custom_lectures/fft_animation/render.py
@@ -1,15 +1,19 @@
 import os
 import sys
 
+
 def render():
     quality = 'l'
     if '-high' in sys.argv:
         quality = 'h'
     if '-4k' in sys.argv:
         quality = 'k'
+    # os.system(f"manim -pq{quality} intro_intro.py IntroIntro")
     # os.system(f"manim -pq{quality} intro.py SineWaves")
     # os.system(f"manim -pq{quality} ft_example.py FourierTransforms")
-    os.system(f"manim -pq{quality} ft_explanation.py FourierTransformInternals")
+    # os.system(f"manim -pq{quality} ft_explanation.py FourierTransformInternals")
+    os.system(f"manim -pq{quality} ft_applications.py Applications")
+
 
 if __name__ == '__main__':
     render()
